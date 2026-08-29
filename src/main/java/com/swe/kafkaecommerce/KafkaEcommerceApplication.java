4e1fa30d47f97e5f30ef1c13f5f9b6ef3d068318
YassYassId <idrissiy371@gmail.com>
2026-08-29 21:11:09 +0100

    Initial commit

---
+++ src/main/java/com/swe/kafkaecommerce/KafkaEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.swe.kafkaecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaEcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaEcommerceApplication.class, args);
+    }
+
+}
