c1bdb35254c8cc3a81b2bd9aa3094b03fbfdc8f9
YassYassId <idrissiy371@gmail.com>
2026-09-07 23:59:06 +0100

    Set the inventory producer topics

--- inventory-service/src/main/java/com/swe/inventoryservice/config/KafkaTopicConfig.java
+++ inventory-service/src/main/java/com/swe/inventoryservice/config/KafkaTopicConfig.java
@@ -9,11 +9,31 @@ public class KafkaTopicConfig {
 
     public static final String ORDER_CREATED_TOPIC = "order.created";
     public static final String ORDER_CREATED_DLT = "order.created.dlt";
+    public static final String INVENTORY_RESERVED_TOPIC = "inventory.reserved";
+    public static final String INVENTORY_REJECTED_TOPIC = "inventory.rejected";
 
     @Bean
     public NewTopic orderCreatedDltTopic() {
         return new NewTopic(
                 ORDER_CREATED_DLT,
+                3,
+                (short) 1
+        );
+    }
+
+    @Bean
+    public NewTopic inventoryReservedTopic() {
+        return new NewTopic(
+                INVENTORY_RESERVED_TOPIC,
+                3,
+                (short) 1
+        );
+    }
+
+    @Bean
+    public NewTopic inventoryRejectedTopic() {
+        return new NewTopic(
+                INVENTORY_REJECTED_TOPIC,
                 3,
                 (short) 1
         );
